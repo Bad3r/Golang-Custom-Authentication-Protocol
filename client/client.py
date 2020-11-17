1e0461f5408892e0c3148892cd63aa4b09ed9c9e
Bad3r <bad3r@pm.me>
2020-11-17 15:48:03 -0500

    commiting initial project setup + test example for a docker-compose client/server env

---
+++ client/client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Import of python system library.
+# This library is used to download the 'index.html' from server.
+# You don't have to install anything special, this library is installed with Python.
+import urllib.request
+
+# This variable contain the request on 'http://localhost:1234/'.
+# You must wondering what is 'http://localhost:1234'?
+# localhost: This means that the server is local.
+# 1234: Remember we define 1234 as the server port.
+fp = urllib.request.urlopen("http://auth_server:1234/")
+
+# 'encodedContent' correspond to the server response encoded ('index.html').
+# 'decodedContent' correspond to the server response decoded (what we want to display).
+encodedContent = fp.read()
+decodedContent = encodedContent.decode("utf8")
+
+# Display the server file: 'index.html'.
+print(decodedContent)
+
+# Close the server connection.
+fp.close()
